ffea70c2e859646a65bc332e486483891bf0b864
Hayden Tomas <goteamtomas@gmail.com>
2025-11-18 19:53:28 +0000

    added nav switcher for centered logo

---
+++ resources/views/sections/headers/logo-top.blade.php
@@ -0,0 +1,53 @@
+{{-- resources/views/sections/headers/logo-top.blade.php --}}
+<header class="site-header header border-b border-white">
+  <div class="max-w-7xl mx-auto px-4 py-4 space-y-4">
+    {{-- Row 1: logo full width, centred (or left if you change classes) --}}
+    <div class="flex justify-center md:justify-center">
+      @if (has_custom_logo())
+        <div class="h-auto w-40">
+          {!! get_custom_logo() !!}
+        </div>
+      @else
+        <a href="{{ home_url('/') }}" class="text-2xl font-semibold text-center">
+          {{ get_bloginfo('name') }}
+        </a>
+      @endif
+    </div>
+
+    {{-- Row 2: same SmartMenus nav as default --}}
+    <nav id="navbar1" class="sm-navbar">
+      <span class="sm-toggler-state" id="sm-toggler-state-1"></span>
+
+      <div class="sm-toggler">
+        <a class="sm-toggler-anchor sm-toggler-anchor--show"
+           href="#sm-toggler-state-1"
+           role="button"
+           aria-label="Open main menu">
+          <span class="sm-toggler-icon sm-toggler-icon--show"></span>
+        </a>
+
+        <a class="sm-toggler-anchor sm-toggler-anchor--hide"
+           href="#"
+           role="button"
+           aria-label="Close main menu">
+          <span class="sm-toggler-icon sm-toggler-icon--hide"></span>
+        </a>
+      </div>
+
+      <div class="sm-collapse">
+        @if (has_nav_menu('primary_navigation'))
+          @php(
+            wp_nav_menu([
+              'theme_location' => 'primary_navigation',
+              'container'      => false,
+              'menu_class'     => 'sm-nav sm-nav--center',
+              'walker'         => new \App\Walkers\SmartMenu_Walker(),
+              'depth'          => 4,
+              'fallback_cb'    => false,
+            ])
+          )
+        @endif
+      </div>
+    </nav>
+  </div>
+</header>
